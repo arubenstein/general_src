a81e2c71409f5b1a95f44486495d85f67a942287
arubenstein <aliza.rubenstein@gmail.com>
2016-11-10 14:44:07 -0500

    Switched script back to cleaved vs. uncleaved and unique sequences

--- seq_proc/SimClUn.py
+++ seq_proc/SimClUn.py
@@ -7,24 +7,27 @@ import conv
 import seq_IO
 import numpy as np
 
-def write_file(outfile, outfile_dist, list_sims, labels, status):
+def write_file(outfile, outfile_dist, list_sims, labels, statuses):
     out = open(outfile, "w")
     out_dist = open(outfile_dist, "w")
     out_dist.write("Seq1,Seq2,Dist\n")
-    for sims,label,stat in zip(list_sims,labels,status):
+    for sims,label,stat in zip(list_sims,labels,statuses):
         out.write("{0},{1}\n".format(label,stat))
         out_dist.write("{0},{1}\n".format(label,stat))
         distances = [ d for s1, s2, d in sims ]
         out.write("Average: {0}\n".format(sum(distances)/len(distances)))
         out.write("Max: {0}\n".format(max(distances)))
         out.write("Min: {0}\n".format(min(distances)))
-        over_25 = sum(1 for d in distances if d >= 0.25)
-        over_50 = sum(1 for d in distances if d >= 0.5)
-        over_75 = sum(1 for d in distances if d >= 0.75)
+        over_25 = len(set([ s1 for s1, s2, d in sims if d >= 0.25 ]))
+        over_50 = len(set([ s1 for s1, s2, d in sims if d >= 0.5 ]))
+        over_75 = len(set([ s1 for s1, s2, d in sims if d >= 0.75 ]))
+        over_90 = len(set([ s1 for s1, s2, d in sims if d >= 0.90 ]))
+
         out.write("N > 0.25 {0}/{1} = {2:.2f}\n".format(over_25,len(distances),float(over_25)/len(distances)))
         out.write("N > 0.50 {0}/{1} = {2:.2f}\n".format(over_50,len(distances),float(over_50)/len(distances)))
         out.write("N > 0.75 {0}/{1} = {2:.2f}\n".format(over_75,len(distances),float(over_75)/len(distances)))
-
+        out.write("N > 0.90 {0}/{1} = {2:.2f}\n".format(over_90,len(distances),float(over_90)/len(distances)))
+	out.write("\n")
         out_dist.write("\n".join("{0},{1},{2}".format(s1,s2,d) for (s1, s2, d) in sims))
         out_dist.write("\n")
 
@@ -32,32 +35,34 @@ def main(list_sequence_names, output_prefix):
     
     list_sequences = [] #list of list of sequences, where each item represents a label 
     labels = [] #labels for list_sequences
-    status = []
+    statuses = []
 
     for [filename, label, stat] in list_sequence_names:
         sequences = seq_IO.read_sequences(filename)
         list_sequences.append(sequences)
         labels.append(label)
-	status.append(stat)
+	statuses.append(stat)
 
-    outfile_cl = '%ssim_cleaved.csv' % (output_prefix)
-    outfile_uncl = '%ssim_uncleaved.csv' % (output_prefix)
+    outfile_cleaved = '%ssim_cleaved.csv' % (output_prefix)
+    outfile_uncleaved = '%ssim_uncleaved.csv' % (output_prefix)
 
-    outfile_dist_cl = '%sdist_sim_cleaved.csv' % (output_prefix)
-    outfile_dist_uncl = '%sdist_sim_uncleaved.csv' % (output_prefix)
+    outfile_dist_cleaved = '%sdist_sim_cleaved.csv' % (output_prefix)
+    outfile_dist_uncleaved = '%sdist_sim_uncleaved.csv' % (output_prefix)
 
-    cleaved_seqs = [ (s,l) for s, l, stat in zip(list_sequences, labels, status) if stat == "CLEAVED" ]
-    uncleaved_seqs = [ (s,l) for s, l, stat in zip(list_sequences, labels, status) if stat == "UNCLEAVED" ]
     cleaved_sims = []
     uncleaved_sims = []
-    for seqs, label in zip(list_sequences, labels):
-	cl = [ seq for s,l in cleaved_seqs for seq in s if l != label ]
-        cleaved_sims.append([(seq1, seq2, 1.0 - conv.hamdist(seq1, seq2, all_threading=True)/float(min(len(seq1),len(seq2)))) for seq1, seq2 in itertools.product(seqs, cl) ] ) 
 
-        uncl = [ seq for s,l in uncleaved_seqs for seq in s if l != label ]
-        uncleaved_sims.append([(seq1, seq2, 1.0 - conv.hamdist(seq1, seq2, all_threading=True)/float(min(len(seq1),len(seq2)))) for seq1, seq2 in itertools.product(seqs, uncl) ] )
-    write_file(outfile_cl, outfile_dist_cl, cleaved_sims, labels, status)
-    write_file(outfile_uncl, outfile_dist_uncl, uncleaved_sims, labels, status)
+    for curr_seqs, curr_label, curr_status in zip(list_sequences, labels, statuses):
+
+	cl = [ seq for s,l,status in zip(list_sequences, labels, statuses) for seq in s if l != curr_label and status == "CLEAVED"  ]
+
+        cleaved_sims.append([(seq1, seq2, 1.0 - conv.hamdist(seq1, seq2, all_threading=True)/float(min(len(seq1),len(seq2)))) for seq1, seq2 in itertools.product(curr_seqs, cl ) ] ) 
+
+        uncl = [ seq for s,l,status in zip(list_sequences, labels, statuses) for seq in s if l != curr_label and status == "UNCLEAVED" ]
+        uncleaved_sims.append([(seq1, seq2, 1.0 - conv.hamdist(seq1, seq2, all_threading=True)/float(min(len(seq1),len(seq2)))) for seq1, seq2 in itertools.product(curr_seqs, uncl ) ] )
+
+    write_file(outfile_cleaved, outfile_dist_cleaved, cleaved_sims, labels, statuses)
+    write_file(outfile_uncleaved, outfile_dist_uncleaved, uncleaved_sims, labels, statuses)
 
 if __name__ == "__main__":
 
